62de74382d67ac382fa05774a24dfa01a5a91c5b
Mohammad <md.nikanjam@gmail.com>
2024-09-22 11:08:27 +0330

    first commit

---
+++ TimerControlOS.cpp
@@ -0,0 +1,31 @@
+#include "TimerControl.h"
+
+
+TimerControl::TimerControl(void)
+{
+    // Save time point at object constructor.
+    T_origin = std::chrono::high_resolution_clock::now();
+}
+
+uint64_t TimerControl::millis(void)
+{
+    std::chrono::time_point<std::chrono::system_clock> T_point;
+    // Get Current time point
+    T_point = std::chrono::high_resolution_clock::now();
+    // Calculate Time duration
+    auto T_dur = std::chrono::duration_cast<std::chrono::milliseconds>(T_point - T_origin);
+    // Return time duration in uint64_t 
+    return static_cast<uint64_t>(T_dur.count());
+}
+
+uint64_t TimerControl::micros(void)
+{
+    std::chrono::time_point<std::chrono::system_clock> T_point;
+    // Get Current time point
+    T_point = std::chrono::high_resolution_clock::now();
+    // Calculate Time duration
+    auto T_dur = std::chrono::duration_cast<std::chrono::microseconds>(T_point - T_origin);
+    // Return time duration in uint64_t 
+    return static_cast<uint64_t>(T_dur.count());
+}
+
---
+++ TimerControlOS.h
@@ -0,0 +1,26 @@
+#ifndef TIMER_CONTROL_H
+#define TIMER_CONTROL_H
+
+#include <chrono>
+
+class TimerControl
+{
+    public:
+
+        // Calculate time from origin time point in milliseconds.
+        uint64_t millis(void);
+
+        // Calculate time from origin time point in microseconds.
+        uint64_t micros(void);
+
+        // Constructor.
+        TimerControl();
+
+    private:
+
+        // Time origin point.
+        std::chrono::time_point<std::chrono::system_clock> T_origin;
+};
+
+
+#endif  // _PRJ_TIMER_H_
